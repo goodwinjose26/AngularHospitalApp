bdaadb9bb42faf048e4848d70ab9d2b906c87951
goodwinjose26 <josegoodwin22@gmail.com>
2022-11-24 10:52:37 +0530

    pateintentry

---
+++ src/app/pateint-entry/pateint-entry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pateint-entry',
+  templateUrl: './pateint-entry.component.html',
+  styleUrls: ['./pateint-entry.component.css']
+})
+export class PateintEntryComponent {
+
+}

4cd37de41f51a51c2859e3f2c566fc0cb573c842
goodwinjose26 <josegoodwin22@gmail.com>
2022-11-24 11:59:10 +0530

    pateintdelete

---
+++ src/app/pateint-delete/pateint-delete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pateint-delete',
+  templateUrl: './pateint-delete.component.html',
+  styleUrls: ['./pateint-delete.component.css']
+})
+export class PateintDeleteComponent {
+
+}

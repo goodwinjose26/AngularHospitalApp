620c1e765a374f90cc52bf7add872f9ebcd2fdb4
goodwinjose26 <josegoodwin22@gmail.com>
2022-11-24 11:49:08 +0530

    pateintsearch

---
+++ src/app/pateint-search/pateint-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pateint-search',
+  templateUrl: './pateint-search.component.html',
+  styleUrls: ['./pateint-search.component.css']
+})
+export class PateintSearchComponent {
+
+}
